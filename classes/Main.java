677f5e49eb181011e676bd57d50b764932d9c5cf
suzuu7 <psuzane91@gmail.com>
2026-04-06 16:37:54 -0300

    mainn

--- classes/Main.java
+++ classes/Main.java
@@ -1,30 +1,18 @@
 public class Main {
-
     public static void main(String[] args) {
-
+       
+        Aluno a1 = new Aluno("João", 17, "10/05/2008", "85999999999", "Carlos");
+        
+        
         Aluno a1 = new Aluno();
-
-        a1.nome = "Maria";
-        a1.idade = 16;
-
-        a1.apresentacao();
-        a1.saudacao();
-    }
-}
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        Aluno a1 = new Aluno();
-
-        a1.nome = "João";
-        a1.idade = 17;
-        a1.dtNascimento = "10/05/2008";
-        a1.telefone = "85999999999";
-        a1.nomeResponsavel = "Carlos";
-
-        a1.apresentacao();
+        a1.setNome("Maria");
+        a1.setIdade(16);
+        a1.setDtNascimento("10/05/2008");
+        a1.setTelefone("85999999999");
+        a1.setNomeResponsavel("Carlos");
+        
+        
+        System.out.println(a1.apresentacao());
         a1.saudacao();
     }
 }
