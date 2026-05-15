faec4b3727bf70c3b24f26f2b99dd50fe46faab5
suzuu7 <psuzane91@gmail.com>
2026-05-14 23:24:25 -0300

    Pessoa class

--- herançaa.java/Pessoa.java
+++ herançaa.java/Pessoa.java
@@ -1,15 +1,31 @@
-class Pessoa {
-    String nome;
-    String fone;
-    String cpf;
+public class Pessoa {
 
-    Pessoa(String nome, String fone, String cpf) {
+    private String nome;
+    private String fone;
+    private String cpf;
+
+    public Pessoa(String nome, String fone, String cpf) {
+
         this.nome = nome;
         this.fone = fone;
         this.cpf = cpf;
     }
 
-    void saudacao() {
-        System.out.println("Olá, meu nome é " + nome);
+    public String getNome() {
+        return nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    // POLIMORFISMO
+    public String saudacao() {
+
+        return "Olá, meu nome é " + nome;
     }
 }
