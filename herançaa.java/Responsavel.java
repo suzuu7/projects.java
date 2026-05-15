0d3ded96412ed422bb5fbfab623207a234571a7e
suzuu7 <psuzane91@gmail.com>
2026-05-14 23:19:38 -0300

    Responsavel

--- herançaa.java/Responsavel.java
+++ herançaa.java/Responsavel.java
@@ -1,8 +1,40 @@
-class Responsavel extends Pessoa {
-    Aluno[] alunos;
+// RESPONSAVEL
 
-    Responsavel(String nome, String fone, String cpf, Aluno[] alunos) {
+public class Responsavel extends Pessoa {
+
+    private Aluno[] alunos;
+
+    public Responsavel(String nome, String fone,
+                       String cpf, Aluno[] alunos) {
+
         super(nome, fone, cpf);
         this.alunos = alunos;
+    }
+
+    public Aluno[] getAlunos() {
+        return alunos;
+    }
+
+    @Override
+    public String saudacao() {
+
+        return "Olá, sou responsável pelos alunos.";
+    }
+
+    public String mostrarAlunos() {
+
+        String texto =
+                "Alunos do responsável "
+                + getNome()
+                + ":\n";
+
+        for (Aluno aluno : alunos) {
+
+            texto += "- "
+                    + aluno.getNome()
+                    + "\n";
+        }
+
+        return texto;
     }
 }
