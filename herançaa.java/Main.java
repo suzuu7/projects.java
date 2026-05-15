10c508d596208eae1b07050c49b01a7b2060a8ba
suzuu7 <psuzane91@gmail.com>
2026-05-14 23:18:25 -0300

    polimorfismo e janela

--- herançaa.java/Main.java
+++ herançaa.java/Main.java
@@ -1,25 +1,76 @@
+// MAIN COM SWING
+
+import javax.swing.JOptionPane;
+
 public class Main {
-    public static void Main(String[] args) {
 
-        Aluno a1 = new Aluno("Ana", "1111-1111", "123", "A01");
-        Aluno a2 = new Aluno("Carlos", "2222-2222", "456", "A02");
+    public static void main(String[] args) {
 
-        Professor prof = new Professor("Marcos", "3333-3333", "789", "P01", "Mestre");
+        // FORMULÁRIO DO ALUNO
+        String nomeAluno =
+                JOptionPane.showInputDialog("Nome do aluno:");
 
-        Funcionario func = new Funcionario("João", "4444-4444", "101", 1, "Secretário");
+        String telefoneAluno =
+                JOptionPane.showInputDialog("Telefone do aluno:");
 
-        Aluno[] lista = {a1, a2};
-        Responsavel resp = new Responsavel("Maria", "5555-5555", "202", lista);
+        String cpfAluno =
+                JOptionPane.showInputDialog("CPF do aluno:");
 
-        a1.saudacao();
-        a1.aprende();
+        String matriculaAluno =
+                JOptionPane.showInputDialog("Matrícula do aluno:");
 
-        prof.saudacao();
-        prof.ensina();
+        // OBJETO ALUNO
+        Aluno a1 = new Aluno(
+                nomeAluno,
+                telefoneAluno,
+                cpfAluno,
+                matriculaAluno
+        );
 
-        func.saudacao();
-        func.desempenhaFuncao();
+        // OBJETOS FIXOS
+        Professor p1 = new Professor(
+                "Pedro",
+                "98888",
+                "456",
+                "002",
+                "Mestre"
+        );
 
-        resp.saudacao();
+        Funcionario f1 = new Funcionario(
+                "Lucas",
+                "97777",
+                "789",
+                "C01",
+                "Coordenador"
+        );
+
+        Aluno[] alunos = {a1};
+
+        Responsavel r1 = new Responsavel(
+                "Carlos",
+                "96666",
+                "999",
+                alunos
+        );
+
+        Pessoa[] pessoas = {a1, p1, f1, r1};
+
+        String resultado = "";
+
+        // POLIMORFISMO
+        for (Pessoa p : pessoas) {
+
+            resultado += p.saudacao() + "\n\n";
+        }
+
+        resultado += r1.mostrarAlunos();
+
+        // JANELA FINAL
+        JOptionPane.showMessageDialog(
+                null,
+                resultado,
+                "Sistema Escolar",
+                JOptionPane.INFORMATION_MESSAGE
+        );
     }
 }
