ff7fc58e502a4e18c343ed94a878e9645d8af037
suzuu7 <psuzane91@gmail.com>
2026-05-14 23:20:34 -0300

    Funcionario

--- herançaa.java/Funcionario.java
+++ herançaa.java/Funcionario.java
@@ -1,14 +1,39 @@
-class Funcionario extends Pessoa {
-    int cod;
-    String funcao;
+// FUNCIONARIO
 
-    Funcionario(String nome, String fone, String cpf, int cod, String funcao) {
+public class Funcionario extends Pessoa {
+
+    private String cod;
+    private String funcao;
+
+    public Funcionario(String nome, String fone,
+                       String cpf, String cod,
+                       String funcao) {
+
         super(nome, fone, cpf);
+
         this.cod = cod;
         this.funcao = funcao;
     }
 
-    void desempenhaFuncao() {
-        System.out.println(nome + " exerce a função de " + funcao);
+    public String getCod() {
+        return cod;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    @Override
+    public String saudacao() {
+
+        return "Olá, sou funcionário e trabalho como "
+                + funcao;
+    }
+
+    public String desempenhaFuncao() {
+
+        return getNome()
+                + " trabalha como "
+                + funcao;
     }
 }
